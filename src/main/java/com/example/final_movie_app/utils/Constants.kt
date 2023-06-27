f8cb66bf74ce04a53fdb20bbd2dc3f7fb7f22ed0
Shashwath BN <76518979+shashwath63@users.noreply.github.com>
2023-06-27 23:31:22 +0530

    Update Constants.kt

--- src/main/java/com/example/final_movie_app/utils/Constants.kt
+++ src/main/java/com/example/final_movie_app/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.final_movie_app.utils
 
 object Constants {
 
-    const val API_KEY = "YOUR_API_KEY"
+    const val API_KEY = "YOUR_API_KEY_HERE"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
