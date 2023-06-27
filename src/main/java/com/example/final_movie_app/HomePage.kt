dc5ffed3205b5576833237c8f58a295f97070a33
shashwath63 <shashwathbn10@gmail.com>
2023-06-27 22:09:36 +0530

    home page added

---
+++ src/main/java/com/example/final_movie_app/HomePage.kt
@@ -0,0 +1,11 @@
+package com.example.final_movie_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
